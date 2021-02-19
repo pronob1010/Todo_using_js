420cdf0893f065841f52e8b63099d3b750bce28d
Pronob <pronobmozumder.info@gmail.com>
2021-02-19 23:56:54 +0600

    filter added

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ let taskInput = document.getElementById("new_task");
 
 // ADD TASK
 form.addEventListener('submit', addtask);
+
 function addtask(e) {
     if (taskInput.value === "") {
         alert('Add Task first...');
@@ -29,28 +30,44 @@ function addtask(e) {
 
 // REMOVE SINGLE TASK
 taskList.addEventListener('click', removeTask);
+
 function removeTask(e) {
     if (e.target.hasAttribute("href")) {
         if (confirm("Are you sure ? ")) {
-            let ele = e.target.parentElement; 
-            ele.remove();// console.log(ele);
-            }
+            let ele = e.target.parentElement;
+            ele.remove(); // console.log(ele);
         }
     }
+}
 
 
 // REMOVE ALL TASK
 clerBtn.addEventListener('click', removeAllTask);
 
-function removeAllTask(e){
+function removeAllTask(e) {
     // console.log("clicked");
     // slower
     // taskList.innerHTML = "";
 
     // faster
-    while(taskList.firstChild){
+    while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
 }
 
 
+// FILTER TASK
+filter.addEventListener('keyup', taskFilter);
+
+function taskFilter(e) {
+    let text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('li').forEach(task => {
+        let item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) === -1) {
+            task.style.display = "none";
+        } else {
+            task.style.display = "block";
+        }
+    });
+}
